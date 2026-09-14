48ee0e9f439b36fb5b3afa210a3c4bf22b9e44d5
Myoui-sys <meninagamer55@gmail.com>
2026-09-14 15:13:38 -0300

    first commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const nome = "Dacy";
+let idade = 23;
+let matriculada = true;
+let nomeExercicio = "Nome"; // Type 'string' is not assignable to type 'number', ou seja, o typescript percebeu que 
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+
+
+const nome: string = "Dacy";
+let idade: number = 23;
+let matriculada: boolean = true;
+
+let nomeExercicio: number = "Nome"; // Type 'string' is not assignable to type 'number', ou seja, o typescript percebeu que 
